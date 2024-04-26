dcf26727b80341503493cbf7795e2252ac8bfe27
Raphael <raphael.mabille@epitech.eu>
2024-04-27 00:55:15 +0200

    fix(encode) Adress all warning

--- src/Encode.hs
+++ src/Encode.hs
@@ -18,7 +18,7 @@ import Lib(Document(..), Header(..), Body(..),
     Container(..),
         Section(name, content),
         List(items),
-        CodeBlock(language, blocks),
+        CodeBlock(..),
     Paragraph(..),
     Element(..),
         Text(text, bold, italic, code),
@@ -44,7 +44,7 @@ indent :: Int -> String
 indent i = replicate (i * 4) ' '
 
 addHeader :: DocumentFormat -> Header -> Int -> String
-addHeader Markdown Header{title, author, date} iLvl =
+addHeader Markdown Header{title, author, date} _ =
     intercalate "\n" [
         "---",
         "title: " ++ title &
@@ -66,37 +66,37 @@ addHeader XML Header{title, author, date} _ =
 
 
 addElement :: DocumentFormat -> Element -> Int -> String
-addElement Markdown (TextElement elem) _ = 
+addElement Markdown (TextElement elemE) _ = 
     let
-        b = if bold elem then "**" else ""
-        i = if italic elem then "*" else ""
-        c = if code elem then "`" else ""
-    in b ++ i ++ c ++ (text elem) ++ c ++ i ++ b
+        b = if bold elemE then "**" else ""
+        i = if italic elemE then "*" else ""
+        c = if code elemE then "`" else ""
+    in b ++ i ++ c ++ (text elemE) ++ c ++ i ++ b
 addElement Markdown (LinkElement link) _ =
     "[" ++ (foldMap (addElement Markdown) (display link) 0) ++ "](" ++ (url link) ++ ")"
 addElement Markdown (ImageElement image) _ =
     "![" ++ (foldMap (addElement Markdown) (alt image) 0) ++ "](" ++ (src image) ++ ")"
-addElement JSON (TextElement elem) iLvl | (bold elem) =
-    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"bold\": \"" ++ (text elem) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
-                                        | (italic elem) =
-    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"italic\": \"" ++ (text elem) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
-                                        | (code elem) =
-    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"code\": \"" ++ (text elem) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
+addElement JSON (TextElement elemE) iLvl | (bold elemE) =
+    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"bold\": \"" ++ (text elemE) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
+                                        | (italic elemE) =
+    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"italic\": \"" ++ (text elemE) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
+                                        | (code elemE) =
+    ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"code\": \"" ++ (text elemE) ++ "\"\n" ++ indent (iLvl) ++ "},\n"
                                         | otherwise =
-    indent (iLvl) ++ "\"" ++ (text elem) ++ "\""
+    indent (iLvl) ++ "\"" ++ (text elemE) ++ "\""
 addElement JSON (LinkElement link) iLvl =
     ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"link\": {\n" ++ indent (iLvl+2) ++ "\"url\": \"" ++ (url link) ++ "\",\n" ++ indent (iLvl+2) ++ "\"content\": [\n" ++ indent (iLvl+3) ++ (foldMap (addElement JSON) (display link) 0) ++ "\n" ++ indent (iLvl+2) ++ "]\n" ++ indent (iLvl+1) ++ "}\n" ++ indent (iLvl) ++ "},\n"
 addElement JSON (ImageElement image) iLvl =
     ",\n" ++ indent iLvl ++ "{\n" ++ indent (iLvl+1) ++ "\"image\": {\n" ++ indent (iLvl+2) ++ "\"url\": \"" ++ (src image) ++ "\",\n" ++ indent (iLvl+2) ++ "\"alt\": [\n" ++ indent (iLvl+3) ++ (foldMap (addElement JSON) (alt image) 0) ++ "\n" ++ indent (iLvl+2) ++ "]\n" ++ indent (iLvl+1) ++ "}\n" ++ indent (iLvl) ++ "},\n"
-addElement XML (TextElement elem) iLvl = 
+addElement XML (TextElement elemE) _ = 
     let
-        b = if bold elem then "**" else ""
-        i = if italic elem then "*" else ""
-        c = if code elem then "`" else ""
-    in b ++ i ++ c ++ (text elem) ++ c ++ i ++ b
-addElement XML (LinkElement link) iLvl =
+        b = if bold elemE then "**" else ""
+        i = if italic elemE then "*" else ""
+        c = if code elemE then "`" else ""
+    in b ++ i ++ c ++ (text elemE) ++ c ++ i ++ b
+addElement XML (LinkElement link) _ =
     "<link url=\"" ++ (url link) ++ "\">" ++ foldMap (addElement XML) (display link) 0 ++ "</link>"
-addElement XML (ImageElement image) iLvl =
+addElement XML (ImageElement image) _ =
     "<image url=\"" ++ (src image) ++ "\">" ++ foldMap (addElement XML) (alt image) 0 ++ "</image>"
 
 
@@ -117,53 +117,54 @@ addListElement XML Paragraph{elements} i =
     "\n" ++ indent (i+1) ++ "<paragraph>" ++ foldMap (addElement XML) elements (i+2) ++ "</paragraph>"
 
 addList :: DocumentFormat -> Container -> Int -> String
-addList Markdown (ListContainer elem) _ =
-    foldMap (addListElement Markdown) (items elem) 0
-addList JSON (ListContainer elem) i =
-    "\n" ++ indent (i+1) ++ "{\n" ++ indent (i+2) ++ "\"list\": [" ++ init (foldMap (addListElement JSON) (items elem) (i+2)) ++ "\n" ++ indent (i+2) ++ "]\n" ++ indent (i+1) ++ "},"
-addList XML (ListContainer elem) i =
-    "\n" ++ indent (i+1) ++ "<list>" ++ foldMap (addListElement XML) (items elem) (i+1) ++ "\n" ++ indent (i+1) ++ "</list>"
-
+addList Markdown (ListContainer elemE) _ =
+    foldMap (addListElement Markdown) (items elemE) 0
+addList JSON (ListContainer elemE) i =
+    "\n" ++ indent (i+1) ++ "{\n" ++ indent (i+2) ++ "\"list\": [" ++ init (foldMap (addListElement JSON) (items elemE) (i+2)) ++ "\n" ++ indent (i+2) ++ "]\n" ++ indent (i+1) ++ "},"
+addList XML (ListContainer elemE) i =
+    "\n" ++ indent (i+1) ++ "<list>" ++ foldMap (addListElement XML) (items elemE) (i+1) ++ "\n" ++ indent (i+1) ++ "</list>"
+addList _ _ _ = ""
 
 addSection :: DocumentFormat -> Container -> Int -> String
-addSection Markdown (SectionContainer elem) i | (name elem) /= "" =
-    "\n\n" ++ replicate i '#' ++ " " ++ (name elem) ++
-    foldMap (diffPC Markdown) (content elem) (i+1)
+addSection Markdown (SectionContainer elemE) i | (name elemE) /= "" =
+    "\n\n" ++ replicate i '#' ++ " " ++ (name elemE) ++
+    foldMap (diffPC Markdown) (content elemE) (i+1)
                                               | otherwise =
-    foldMap (diffPC Markdown) (content elem) (i+1)
-addSection JSON (SectionContainer elem) i =
+    foldMap (diffPC Markdown) (content elemE) (i+1)
+addSection JSON (SectionContainer elemE) i =
     "\n" ++
     indent (i+1) ++ "{\n" ++
     indent (i+2) ++ "\"section\": {\n" ++
-    indent (i+3) ++ "\"title\": \"" ++ (name elem) ++ "\",\n" ++
+    indent (i+3) ++ "\"title\": \"" ++ (name elemE) ++ "\",\n" ++
     indent (i+3) ++ "\"content\": [" ++
-    init (foldMap (diffPC JSON) (content elem) (i+3)) ++ "\n" ++
+    init (foldMap (diffPC JSON) (content elemE) (i+3)) ++ "\n" ++
     indent (i+3) ++ "]\n" ++
     indent (i+2) ++ "}\n" ++
     indent (i+1) ++ "}\n"
-addSection XML (SectionContainer elem) i =
+addSection XML (SectionContainer elemE) i =
     "\n" ++
-    indent (i+1) ++ "<section title=\"" ++ (name elem) ++ "\">" ++
-    foldMap (diffPC XML) (content elem) (i+1) ++ "\n" ++
+    indent (i+1) ++ "<section title=\"" ++ (name elemE) ++ "\">" ++
+    foldMap (diffPC XML) (content elemE) (i+1) ++ "\n" ++
     indent (i+1) ++ "</section>"
+addSection _ _ _ = ""
 
 
 addCodeBlock :: DocumentFormat -> Container -> Int -> String
-addCodeBlock Markdown (CodeBlockContainer elem) iLvl =
-    "\n\n```" ++ foldMap (addParagraph Markdown) (blocks elem) iLvl ++ "\n```"
-addCodeBlock JSON (CodeBlockContainer elem) iLvl =
+addCodeBlock Markdown (CodeBlockContainer elemE) iLvl =
+    "\n\n```" ++ foldMap (addParagraph Markdown) (blocks elemE) iLvl ++ "\n```"
+addCodeBlock JSON (CodeBlockContainer elemE) iLvl =
     "\n" ++
     indent (iLvl+1) ++ "{\n" ++
     indent (iLvl+2) ++ "\"codeblock\": [\n" ++
-    init (foldMap (addParagraph JSON) (blocks elem) (iLvl+2)) ++ "\n" ++
+    init (foldMap (addParagraph JSON) (blocks elemE) (iLvl+2)) ++ "\n" ++
     indent (iLvl+2) ++ "]" ++ "\n" ++
     indent (iLvl+1) ++ "},"
-addCodeBlock XML (CodeBlockContainer elem) iLvl =
+addCodeBlock XML (CodeBlockContainer elemE) iLvl =
     "\n" ++
     indent (iLvl+1) ++ "<codeblock>\n" ++
-    foldMap (addParagraph XML) (blocks elem) (iLvl+1) ++ "\n" ++
+    foldMap (addParagraph XML) (blocks elemE) (iLvl+1) ++ "\n" ++
     indent (iLvl+1) ++ "</codeblock>"
-
+addCodeBlock _ _ _ = ""
 
 addContainer :: DocumentFormat -> Container -> Int -> String
 addContainer form cont@(SectionContainer _) i = addSection form cont i
