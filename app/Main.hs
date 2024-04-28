ccdb80d20a6b608d75f709a4164fec19cd9854f8
lallain <lea.allain@epitech.eu>
2024-04-28 18:17:23 +0200

    feat(decode) Add xml decode header but it is incomplet

--- app/Main.hs
+++ app/Main.hs
@@ -14,18 +14,11 @@ import Conf (confParser, Conf(..))
 import ConfUtils (getFormat)
 import Encode (encode)
 import Decode (decode)
-import Parsing
-
-printResult :: Maybe (String, String) -> IO()
-printResult (Just (x, y)) = putStrLn $ "(Just (" ++ show x ++ ", " ++ show y ++ "))"
-printResult Nothing = putStrLn "Nothing"
 
 main :: IO ()
 main = do
     conf <- execParser (info (confParser <**> helper) $ failureCode 84)
     content <- readFile $ inputFile conf
-    let result = parseSome (parseAnyChar ['<', 'd', 'o', 'c', 'u', 'm', 'e', 'n', 't','>']) content
-    printResult result
     case decode (getFormat conf content) content of
         Nothing -> exitWith $ ExitFailure 84
         Just document -> print document >> (putStrLn $ encode conf document)
--- src/Decode.hs
+++ src/Decode.hs
@@ -12,4 +12,28 @@ import Lib(Document(Document), Header(Header), Body(Body))
 
 decode :: Conf -> String -> Maybe Document
 decode Conf{inputFormat=Nothing} _ = Nothing
-decode _ _ = Just $ Document (Header "Document" Nothing Nothing) (Body [])
+decode _ content = Just $ Document (Header (getXMLTitle content) (getXMLAuthors content) (getXMLDate content)) (Body [])
+
+getXMLTitle :: String -> String
+getXMLTitle content =
+    let startTag = "<header title=\""
+        endTag = "\">"
+        authorsStart = drop (length startTag) $ snd $ break (== startTag) $ words content
+        authorsEnd = takeWhile (/= endTag) authorsStart
+    in unwords authorsEnd
+
+getXMLAuthors :: String -> Maybe String
+getXMLAuthors content =
+    let startTag = "<author>"
+        endTag = "<\\author>"
+        authorsStart = drop (length startTag) $ snd $ break (== startTag) $ words content
+        authorsEnd = takeWhile (/= endTag) authorsStart
+    in if null authorsEnd then Nothing else Just (unwords authorsEnd)
+
+getXMLDate :: String -> Maybe String
+getXMLDate content =
+    let startTag = "<date>"
+        endTag = "<\\date>"
+        authorsStart = drop (length startTag) $ snd $ break (== startTag) $ words content
+        authorsEnd = takeWhile (/= endTag) authorsStart
+    in if null authorsEnd then Nothing else Just (unwords authorsEnd)
